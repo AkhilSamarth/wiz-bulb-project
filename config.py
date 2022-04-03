153b2d0bde0f376359d7856e7618da043d88bdc7
Akhil Samarth <akhilsamarth236@gmail.com>
2022-04-02 20:38:10 -0400

    Added functions to get datetime info from config

--- config.py
+++ config.py
@@ -1,11 +1,25 @@
+import datetime as dt
 import json
+from typing import List
 
 
 with open("config.json") as f:
     CONFIG = json.load(f)
 
 
-def get_bulb_ips() -> list:
+def _get_current_year_dst_record() -> dict:
+    current_year = dt.datetime.now().year
+
+    dst_records = CONFIG["daylightSavingTime"]
+
+    for record in dst_records:
+        if record["year"] == current_year:
+            return record
+
+    raise Exception(f"Missing DST config for current year: {current_year}")
+
+
+def get_bulb_ips() -> List[str]:
     return CONFIG["bulbIps"]
 
 
@@ -15,3 +29,28 @@ def get_bulb_port() -> int:
 
 def get_socket_port() -> int:
     return CONFIG["socketPort"]
+
+
+def get_sunset_times() -> List[dt.datetime]:
+    raw_data = CONFIG["sunsetTimes"]
+    current_year = dt.datetime.now().year
+
+    sunset_times = []
+    for record in raw_data:
+        combined_datetime = f"{record['date']}-{record['time']}"
+
+        sunset_times.append(dt.datetime.strptime(combined_datetime, "%m/%d-%H:%M").replace(year=current_year))
+    
+    return sorted(sunset_times)
+
+
+def get_dst_start_date() -> dt.date:
+    dst_record = _get_current_year_dst_record()
+
+    return dt.datetime.strptime(dst_record["start"], "%m/%d").date().replace(year=dst_record["year"])
+
+
+def get_dst_end_date() -> dt.date:
+    dst_record = _get_current_year_dst_record()
+
+    return dt.datetime.strptime(dst_record["end"], "%m/%d").date().replace(year=dst_record["year"])
