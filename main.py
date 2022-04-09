9c0937b41d7f6950d0c526811ed7b6d5f773c618
Akhil Samarth <akhilsamarth236@gmail.com>
2022-04-09 19:48:56 -0400

    Added main.py

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-import config
+import argparse
+
+import controller
+
+
+def parse_args() -> dict:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--temp", type=int, help="temperature of bulbs, must provide brightness if this is included")
+    parser.add_argument("--brightness", type=int, help="brightness of bulbs, must provide temp if this is included")
+    parser.add_argument("--bulbs-off", action="store_true", help="turn bulbs off")
+    parser.add_argument("--bulbs-on", action="store_true", help="turns bulbs on")
+    parser.add_argument("--auto-set", action="store_true", help="automatically determine correct options based on current time and config")
+
+    return vars(parser.parse_args())
+
+
+def main():
+    args = parse_args()
+
+    if args["bulbs_off"]:
+        controller.set_state(False)
+    elif args["bulbs_on"]:
+        controller.set_state(True)
+    elif args["temp"] and args["brightness"]:
+        controller.set_temp_and_brightness(temp=args["temp"], brightness=args["brightness"])
+    elif args["auto_set"]:
+        controller.set_bulb_based_on_timeline()
 
 
 if __name__ == "__main__":
-    print(config.get_dst_start_date())
-    print(config.get_dst_end_date())
+    main()
